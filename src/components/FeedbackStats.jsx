f21a4cef4a4fc67eb2a9ec2d48cf708dafb82ea0
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-03-20 19:11:18 +0000

    uptade and finish

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 function FeedbackStats({ feedBack }) {
-  let average = feedBack.reduce((acc, cur) => {
-    return acc + cur.rating;
-  }, 0)/feedBack.length
+  let average =
+    feedBack.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedBack.length;
 
-  console.log(average);
+  average=average.toFixed(1).replace(/[.,]0$/,'')
 
   return (
     <div className="feedback-stats">
-      <h4 className="feedback-reviews"> {feedBack.length} Reviews </h4>
-      <h4>Average Ratings : {average}</h4>
+      <h4 className="feedback-reviews "> {feedBack.length} Reviews </h4>
+      <h4 className="feedback-average">Average Ratings : {isNaN(average) ? 0 : average}</h4>
     </div>
   );
 }
